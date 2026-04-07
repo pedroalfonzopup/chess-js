a9178dc804aedb55417648c3fd1b8c16f78d9691
Pedro Alfonzo <pedroalfonzopup@gmail.com>
2026-04-07 15:31:49 -0300

    Added pieces, squares and CSS with SASS

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const gameBoard = document.querySelector("#gameboard")
 const playerTurn = document.querySelector("#player-turn")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8
+const lenght = 8
 
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-    '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
@@ -16,8 +16,24 @@ const startPieces = [
 ]
 
 function createBoard() {
-    startPieces.forEach((piece) => {
+    startPieces.forEach((piece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
+        square.innerHTML = piece
+        square.setAttribute('square-id', i)
+
+        const row = Math.floor( (63 - i) / 8 ) + 1
+        if ( row % 2 === 0 ) {
+            square.classList.add(i % 2 === 0 ? "white" : "black")
+        } else {
+            square.classList.add(i % 2 === 0 ? "black" : "white")
+        }
+
+        if ( i <= 15 ) {square.firstChild.firstChild.classList.add('black-piece')} 
+        if ( i >= 48 ) {square.firstChild.firstChild.classList.add('white-piece')}
+
+        gameBoard.append(square)
     })
 }
+
+createBoard()
